3cb0a84a4ad3f0eb18ff010d3db3501022f34b54
ppk150 <67466148+ppk150@users.noreply.github.com>
2022-07-01 18:08:14 +0900

    20220701_반복분(for문)_ 구구단 문제
    문제
    N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
    
    입력
    첫째 줄에 N이 주어진다. N은 1보다 크거나 같고, 9보다 작거나 같다.
    
    출력
    출력형식과 같게 N*1부터 N*9까지 출력한다

---
+++ GuGu.java
@@ -0,0 +1,19 @@
+public class GuGu {
+
+    public void dan(int n){
+
+        for (int i = 1 ; i < 10 ; i++){
+            System.out.println(n*i);
+        }
+    }
+
+
+    public static void main(String[] args){
+        int n = 3 ;
+        GuGu gugu = new GuGu();
+
+        for (int i = 2; i < 10 ; i++){
+            gugu.dan(i);
+        }
+    }
+}
