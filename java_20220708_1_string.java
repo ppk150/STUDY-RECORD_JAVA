1cd9dcb006d7d2655b8c6678e2a325559401f5dc
ppk150 <67466148+ppk150@users.noreply.github.com>
2022-07-08 00:05:35 +0900

    20220708_문자열(string)_아스키 코드
    문제
    알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
    
    입력
    알파벳 소문자, 대문자, 숫자 0-9 중 하나가 첫째 줄에 주어진다.
    
    출력
    입력으로 주어진 글자의 아스키 코드 값을 출력한다.

---
+++ java_20220708_1_string.java
@@ -0,0 +1,23 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class java_20220708_1_string {
+    public static void main(String[] args) {
+
+        InputStream in = System.in;
+
+        Scanner sc = new Scanner(System.in);
+
+        String A = new String();
+
+        A = sc.next();
+
+        A.charAt(0);
+
+        int B = A.charAt(0);
+
+        System.out.println(B);
+
+
+    }
+}
