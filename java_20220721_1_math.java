af3b3502272c831ab776b3d13059669d2d01c1c9
ppk150 <67466148+ppk150@users.noreply.github.com>
2022-07-25 09:34:42 +0900

    20220725_기본수학_소인수분해
    문제
    정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 정수 N (1 ≤ N ≤ 10,000,000)이 주어진다.
    
    출력
    N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다. N이 1인 경우 아무것도 출력하지 않는다.
    
    문제출처 : https://www.acmicpc.net/problem/11653

---
+++ java_20220721_1_math.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class java_20220721_1_math {
+    public static void main(String[] args)throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String AF = bf.readLine();
+        int A = Integer.parseInt(AF);
+        int[] B = new int[A];
+        Arrays.fill(B, 1);
+
+
+
+        int j ;
+        int ij ;
+        int count = 0;
+        int count_2 = 0;
+        int end = 0;
+        int num =A;
+
+        int[] ERA = new int[A+1];
+        Arrays.fill(ERA, 1);
+        ERA[0] = 0;
+        ERA[1] =0;
+        int L = ERA.length;
+
+        for (int i = 2 ; i < A ; i++){
+            if (ERA[i] == 1){
+                j=2;
+                while (i*j <= A){
+                    ij = i*j;
+                    ERA[ij]=0;
+                    j +=1;
+
+                }
+            }
+
+        }
+        int[] sosu = new int[L]; // 입력된 숫자 이하의 소수 다 저장
+
+        for (int k = B[0]; k < L; k++){
+            if (ERA[k]==1){
+                sosu[count] = k;
+                count++;
+            }
+
+        }
+        int sosuL = sosu.length;
+
+
+        while(end ==0){
+
+            if (sosu[count_2] ==0){
+                break;
+            }else if (num%sosu[count_2]==0){
+                num = num/sosu[count_2];
+                System.out.println(sosu[count_2]);
+            }else {
+                count_2++;
+            }
+
+        }
+
+
+    }
+}
