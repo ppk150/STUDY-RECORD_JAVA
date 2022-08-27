9c506c1d631e60256f481eeb04fa27b61a0aba27
S2-CHICKEN <67466148+ppk150@users.noreply.github.com>
2022-08-27 12:06:06 +0900

    20220827_엘리스아카데미_레벨1_좋아하는 카드의 개수 2
    엘리스는 여가 시간에 카드 놀이를 즐겨합니다. 엘리스는 N×MN \times MN×M개의 카드를 가지고 있고, 그 중 특히 KKK가 적혀있는 카드를 좋아합니다. 카드는 책상 위에 NNN행 MMM열로 나열되어있습니다.
    
    엘리스가 가지고 있는 카드 중 KKK가 적혀있는 카드가 몇 개인지 구해주세요!
    
    
    입력
    첫째 줄에 카드가 놓인 행, 열의 개수 N,MN, MN,M이 주어집니다. (1≤N,M≤1001 \leq N, M \leq 1001≤N,M≤100)
    둘째 줄부터 NNN개의 줄에 걸쳐, 각 줄마다 카드에 쓰인 수를 나타내는 MMM개의 자연수 Ai,jA_{i, j}A 
    i,j
    ​
     가 공백으로 구분되어 주어집니다. (1≤Ai,j≤1,0001 \leq A_{i, j} \leq 1,0001≤A 
    i,j
    ​
     ≤1,000)
    다음 줄에 엘리스가 좋아하는 카드에 쓰여있는 수 KKK가 주어집니다. (1≤K≤1,0001 \leq K \leq 1,0001≤K≤1,000)
    
    출력
    첫째 줄에 엘리스가 가지고있는 카드 중 KKK가 쓰여있는 카드의 개수를 출력합니다.
    
    [예제 입력 1]
    3 4
    3 1 4 1
    5 9 2 6
    5 3 5 8
    5
    Copy
    [예제 출력 1]
    3

---
+++ java_20220810_5_num.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class java_20220810_5_num {
+    public static void main(String[] args) {
+        int x;
+        Scanner sc = new Scanner(System.in);
+
+        int count = 0;
+        int count_x = 0 ;
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] ALL = new int[N*M];
+
+
+        for (int i = 0 ; i < N ; i++){
+            for(int j = 0 ; j < M ; j++){
+                ALL[count] = sc.nextInt();
+                count++;
+            }
+
+        }
+        x = sc.nextInt();
+
+
+        for(int i = 0 ; i < N*M ; i++){
+            if(ALL[i]==(x)){
+                count_x++;
+            }
+
+        }
+        System.out.println(count_x);
+    }
+}
